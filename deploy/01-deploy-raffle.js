02720b4e428eab076ebbffe0290a3a674e9855bc
Bishal Sunuwar <bishalsunuwar24@gmail.com>
2022-11-21 13:06:25 +0545

    deployed

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,15 +1,17 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
 const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
     let vrfCoordinatorV2Address, subscriptionId
 
     if (developmentChains.includes(network.name)) {
-        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoodinatorV2Mock")
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address
         const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
         const transactionReceipt = await transactionResponse.wait(1)
@@ -41,4 +43,11 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        console.log("verifying......")
+        await verify(raffle.address, args)
+    }
+    log("--------------------------------------------------------------------")
 }
+
+module.exports.tag = ["all", "raffle"]
---
+++ utils/verify.js
@@ -0,0 +1,21 @@
+const { run } = require("hardhat")
+
+const verify = async (contractAddress, args) => {
+    console.log("verifying contract")
+    try {
+        await run("veriy:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified....")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+module.exports = {
+    verify,
+}
